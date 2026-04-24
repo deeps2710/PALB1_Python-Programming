6fb0c705ab46628af9a15e35e8a88cbef40749b2
Deepshikha Rani <deepshikharani297@gmail.com>
2026-04-24 19:00:08 +0530

    Add prevSmaller function to find previous smaller elements
    Implement a function to find previous smaller elements.

---
+++ previous_smaller_element.py
@@ -0,0 +1,15 @@
+class Solution:
+	def prevSmaller(self, arr):
+		stack=[]
+		result=[]
+	    for x in arr:
+	       while stack and stack[-1]>=x:
+	           stack.pop()
+	       if not stack:
+	           result.append(-1)
+	       else:
+	           result.append(stack[-1])
+	       
+	       stack.append(x)
+	       
+	    return result
